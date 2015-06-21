eba67885671c72379b6d7a461e2de1964d67cc1d
Raitis Stengrevics <dagrevis@gmail.com>
2015-06-21 20:29:26 +0300

    Update on changes only

--- update.sh
+++ update.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-rm -rf _site/
-git pull -q
-bundle install
-git checkout Gemfile.lock
-bundle exec jekyll build
+OLD_HASH=$(git rev-parse --short HEAD)
+git pull
+if [[ "${OLD_HASH}" != $(git rev-parse HEAD) ]]; then
+    rm -rf _site/
+    bundle install
+    git checkout Gemfile.lock
+    bundle exec jekyll build
+fi
