746a2f268f3699f6b20e184c8cb4ea6aa57f8400
Raitis Stengrevics <dagrevis@gmail.com>
2015-07-09 20:58:39 +0300

    Fix problems with opening links

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,5 +1,11 @@
 $(function() {
-    $("#body a").each(function() {
-	$(this).attr("target", "_blank")
+    $("#body article section a").each(function() {
+	var $anchor = $(this)
+	var href = $anchor.attr("href")
+	var link = href.split("//")[1] || null
+	if (link && link.indexOf("dagrev.is") !== 0) {
+	    $anchor.attr("target", "_blank")
+	}
+
     })
 })
