9a0e5f1c16717a2fc529ac36fd83a916e0d8df64
Raitis Stengrevics <dagrevis@gmail.com>
2016-04-09 22:04:48 +0300

    I had tabs in my code!

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -2,18 +2,18 @@ $(function() {
     FastClick.attach(document.body)
 
     $("#body article section a").each(function() {
-	var $anchor = $(this)
-	var href = $anchor.attr("href")
-	var link = href.split("//")[1] || null
-	if (link && link.indexOf("dagrev.is") !== 0) {
-	    $anchor.attr("target", "_blank")
-	}
+        var $anchor = $(this)
+        var href = $anchor.attr("href")
+        var link = href.split("//")[1] || null
+        if (link && link.indexOf("dagrev.is") !== 0) {
+            $anchor.attr("target", "_blank")
+        }
     })
 
     // Written like this to avoid bots getting my... you know what.
     var dont = "me"
     var spam = "dagrev.is"
     $("#do-not-spam-this").html(
-	dont + String.fromCharCode(Math.pow(4, 3)) + spam
+        dont + String.fromCharCode(Math.pow(4, 3)) + spam
     )
 })
