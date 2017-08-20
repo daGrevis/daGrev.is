f9f3fab452759b517520f72932bfca47e3e3abc4
Raitis Stengrevics <raitis.stengrevics@orangelv.com>
2017-08-20 20:06:15 +0300

    Get rid of jQuery dependency

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,18 +1,20 @@
-$(function() {
-    // Adds target=_blank to all links that don't start with dagrev.is.
-    $("#body article section a").each(function() {
-        var $anchor = $(this)
-        var href = $anchor.attr("href")
-        var link = href.split("//")[1] || null
-        if (link && link.indexOf("dagrev.is") !== 0) {
-            $anchor.attr("target", "_blank")
-        }
-    })
+document.addEventListener('DOMContentLoaded', function() {
+  // Adds target=_blank to all links that don't start with 'dagrev.is'.
+  document.querySelectorAll('#body article section a').forEach(function(node) {
+    var href = node.href
+    var link = href.split('//')[1]
+    if (link && link.indexOf('dagrev.is') !== 0) {
+      node.rel = 'noopener'
+      node.target = '_blank'
+    }
+  })
 
-    // Written like this to avoid bots getting my... you know what.
-    var dont = "dagrevis"
-    var spam = ["om", ".c", "ail", "gm"].reverse().join("")
-    $("#do-not-spam-this").html(
-        dont + String.fromCharCode(Math.pow(4, 3)) + spam
-    )
+  // Written like this to avoid bots getting my... you know what.
+  var doNotSpamThisNode = document.getElementById('do-not-spam-this')
+  if (doNotSpamThisNode) {
+    var dont = 'dagrevis'
+    var spam = ['om', '.c', 'ail', 'gm'].reverse().join('')
+
+    doNotSpamThisNode.innerText = dont + String.fromCharCode(Math.pow(4, 3)) + spam
+  }
 })
